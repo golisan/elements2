f3c327f99e77c1516cc194611b3290abb2be4127
Markus Golob <Markus.Golob@IuK-FZ.justiz.bwl.de>
2020-02-10 08:12:13 +0100

    First commit

---
+++ src/components/WaterCan.vue
@@ -0,0 +1,46 @@
+<template>
+  <span @click="onClick($event)">
+    <img style="width: 100px; height: 100px" :src="fillImage" />
+  </span>
+</template>
+
+<script>
+import fullGlas from "@/assets/images/waterglasfull.png";
+import emptyGlas from "@/assets/images/waterglasempty.png";
+export default {
+  name: "WaterCan",
+  props: {
+    value: {
+      type: Number,
+      required: true,
+      default: 0
+    }
+  },
+  mounted() {},
+  data() {
+    return {};
+  },
+  watch: {
+    value(val) {
+      console.log("watch", val);
+    }
+  },
+  computed: {
+    fillImage() {
+      return this.value === 100 ? fullGlas : emptyGlas;
+    }
+  },
+  methods: {
+    onClick() {
+      if (this.value === 0) {
+        this.$emit("input", 100);
+      } else {
+        this.$emit("input", 0);
+      }
+    }
+  }
+};
+</script>
+
+<style scoped>
+</style>
---
+++ src/views/Elements.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <WaterCan v-model="currentState"></WaterCan>
+
+    <div>
+      <h1>Ich habe heute {{ ml }} ml getrunken</h1>
+    </div>
+
+    <i style="font-size: 50px; color: red;" class="el-icon-close" />
+  </div>
+</template>
+
+<script>
+import WaterCan from "../components/WaterCan";
+export default {
+  name: "Elements",
+  components: { WaterCan },
+  data: function() {
+    return {
+      ml: 0,
+      currentState: 0
+    };
+  },
+
+  watch: {
+    currentState(newValue, oldValue) {
+      if (oldValue < newValue) this.ml = this.ml + 100;
+      else this.ml = this.ml - 100;
+    }
+  }
+};
+</script >
+
+<style scoped>
+.empty {
+  background-color: white;
+}
+.full {
+  background-color: blue;
+}
+</style>
